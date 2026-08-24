d8658c84be9a3a354957116f9f0aacdc06d4d29e
Rafa130206 <rafabompadre@gmail.com>
2026-08-24 11:12:23 -0300

    created module structure

--- notification-service/src/test/java/com/fiapon/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/fiapon/notification/NotificationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fiapon.tc3;
+package com.fiapon.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Tc3ApplicationTests {
+class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
