98aa58ebcd50c979aab46c6a76d2fc31b2956e21
Rafa130206 <rafabompadre@gmail.com>
2026-08-24 09:53:23 -0300

    first commit

---
+++ src/main/java/com/fiapon/tc3/Tc3Application.java
@@ -0,0 +1,13 @@
+package com.fiapon.tc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiapon/tc3/Tc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiapon.tc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
