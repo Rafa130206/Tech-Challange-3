d715347553691c2f6675b956bd1bb0b64914ee58
Rafael-Lima <162456786+Rafa130206@users.noreply.github.com>
2026-08-30 19:52:58 -0300

    Revert "History Service Implementation"

--- history-service/src/main/java/com/fiapon/history/exceptions/HistoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fiapon.history.exceptions;
-
-public class HistoryNotFoundException extends RuntimeException {
-    public HistoryNotFoundException(Long id) {
-        super("History not found: " + id);
-    }
-}
